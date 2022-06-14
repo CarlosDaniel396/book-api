ce17c4cd1a0ecf7be49763a262e42d7952478d11
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-06-14 16:32:39 -0300

    feat: create-book

--- backend/src/main/java/com/carlos/demo/services/BookService.java
+++ backend/src/main/java/com/carlos/demo/services/BookService.java
@@ -8,8 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.carlos.demo.dto.AuthorDTO;
 import com.carlos.demo.dto.BookDTO;
+import com.carlos.demo.entities.Author;
 import com.carlos.demo.entities.Book;
+import com.carlos.demo.repositories.AuthorRepository;
 import com.carlos.demo.repositories.BookRepository;
 import com.carlos.demo.services.exceptions.ResourceNotFoundException;
 
@@ -18,6 +21,9 @@ public class BookService {
 
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private AuthorRepository authorRepository;
 
 	@Transactional(readOnly = true)
 	public Page<BookDTO> findAllPaged(Pageable pageRequest) {
@@ -31,5 +37,25 @@ public class BookService {
 		Book entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new BookDTO(entity, entity.getAuthors());
 	}
+	
+	@Transactional
+	public BookDTO insert(BookDTO dto) {
+		Book entity = new Book();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new BookDTO(entity);
+	}
 
+	private void copyDtoToEntity(BookDTO dto, Book entity) {
+
+		entity.setName(dto.getName());
+		entity.setEdition(dto.getEdition());
+		entity.setPublicationYear(dto.getPublicationYear());
+
+		entity.getAuthors().clear();
+		for (AuthorDTO authorDTO : dto.getAuthors()) {
+			Author author = authorRepository.getReferenceById(authorDTO.getId());
+			entity.getAuthors().add(author);
+		}
+	}
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,10 +1,14 @@
 INSERT INTO tb_author (name) VALUES ('Luciano Ramalho');
-INSERT INTO tb_author (name) VALUES ('Luciano Ramalho');
 INSERT INTO tb_author (name) VALUES ('Osvaldo Santana Neto');
 INSERT INTO tb_author (name) VALUES ('Brian K. Jones');
 INSERT INTO tb_author (name) VALUES ('Chetan Giridhar');
+INSERT INTO tb_author (name) VALUES ('Paul Deitel');
 
 INSERT INTO tb_book (name, edition, publication_year) VALUES ('Python Fluente',2,2015);
+INSERT INTO tb_book (name, edition, publication_year) VALUES ('Java Como Programar',5,1996);
+
 
 INSERT INTO tb_book_author (book_id, author_id) VALUES(1, 1);
+INSERT INTO tb_book_author (book_id, author_id) VALUES(2, 5);
+
 
