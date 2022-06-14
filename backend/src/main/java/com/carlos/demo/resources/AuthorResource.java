6511caa278f55340210915929a9cdc2957ee4ed8
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-06-14 04:16:42 -0300

    feat: find-author-by-id

--- backend/src/main/java/com/carlos/demo/resources/AuthorResource.java
+++ backend/src/main/java/com/carlos/demo/resources/AuthorResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class AuthorResource {
 	public ResponseEntity<Page<AuthorDTO>> findAll(Pageable pageable) {
 		Page<AuthorDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AuthorDTO> findById(@PathVariable Long id) {
+		AuthorDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
